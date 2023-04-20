18eadf81b5c5217a897ecd1fea907fad01959ea0
jsus <esseswann@gmail.com>
2023-04-20 11:55:59 +0300

    Using nextHandler

--- src/extractOutput/sex.ts
+++ src/extractOutput/sex.ts
@@ -1,6 +1,7 @@
 export type Token = "(" | ")" | " " | "'";
 
 const parseSex = (str: Token[], current = 0): number => {
+  let handler = undefined;
   let symbol = "";
   for (let i = current + 1; i < str.length; i++) {
     const token = str[i];
@@ -9,10 +10,18 @@ const parseSex = (str: Token[], current = 0): number => {
         i = parseSex(str, i);
         break;
       case ")":
-        if (symbol) processSymbol(symbol);
+        if (symbol) {
+          const nextHandler = handlers.get(symbol);
+          if (nextHandler) handler = nextHandler();
+          else if (handler) handler.append(symbol);
+        }
         return i;
       case " ":
-        if (symbol) processSymbol(symbol);
+        if (symbol) {
+          const nextHandler = handlers.get(symbol);
+          if (nextHandler) handler = nextHandler();
+          else if (handler) handler.append(symbol);
+        }
         symbol = "";
         break;
       case "'":
@@ -21,18 +30,30 @@ const parseSex = (str: Token[], current = 0): number => {
         symbol += token;
     }
   }
+  if (handler) console.log(handler.build);
   return current;
 };
 
-const processSymbol = (symbol: string) => {
-  switch (symbol) {
-    case "let":
-      console.log("let");
-      break;
+const handlersRaw: Record<string, Handler> = {
+  let: () => {
+    const result: Record<string, boolean> = {};
+    return {
+      append(symbol) {
+        console.log(symbol);
+        result[symbol] = true;
+      },
+      build() {
+        return result;
+      },
+    };
+  },
+};
 
-    default:
-      break;
-  }
+const handlers: Map<string, Handler> = new Map(Object.entries(handlersRaw));
+
+type Handler = () => {
+  append: (symbol: string) => void;
+  build: () => void;
 };
 
 export default parseSex;
